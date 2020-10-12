0e89ff3874fefd5f389f14b2d447203429510039
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-10-12 11:40:10 +0700

    form validation change password

--- README.md
+++ README.md
@@ -1,24 +1,77 @@
-# frontend-zwallet
+# Zwallet
 
-## Project setup
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Getting Started](#getting-started)
+  * [Installation](#installation)
+* [Screenshots](#screenshots)
+* [Related Project](#related-project)
+* [Contact](#contact)
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+
+Zwallet is a application too help people about payment
+
+### Built With
+
+* [VueJS](http://vuejs.org/)
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+### Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/alanard/frontend-zwallet.git
 ```
+2. Install NPM packages
+```sh
 npm install
 ```
-
-### Compiles and hot-reloads for development
+3. Add .env file at root folder project, and add following
+```sh
+VUE_APP_ENDPOINT = [Backend API]
+VUE_APP_SECRET_KEY = your_secret_key
 ```
+4. Compiles and hot-reloads for development
+```sh
 npm run serve
 ```
-
-### Compiles and minifies for production
+5. Lints and fixes files
+```
+npm run lint
+```
+6. Compiles and minifies for production
 ```
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+<!-- ROADMAP -->
+## Screenshots
+
+<!-- <p align='center'>
+  <span>
+      <image width="200" src='./screenshots/landing.png' />
+      <image width="200" src='./screenshots/login.png' />
+      <image width="200" src='./screenshots/register.png' />
+      <image width="200" src='./screenshots/home.png' />
+      <image width="200" src='./screenshots/cart.png' />
+      <image width="200" src='./screenshots/chart.png' />
+      <image width="200" src='./screenshots/order.png' />
+      <image width="200" src='./screenshots/product.png' />
+      <image width="200" src='./screenshots/user.png' />
+      <image width="200" src='./screenshots/checkout.png' />
+  </span>
+ </p> -->
+ 
+## Related Project
+<!-- * [`ark-backend`](https://github.com/erlanggajatikusuma/ark-backend) -->
+
+
--- src/views/main/home/profile/change-pw.vue
+++ src/views/main/home/profile/change-pw.vue
@@ -26,25 +26,27 @@
                         <div class="form-group">
                             <div class="input-group">
                                 <i class="satu fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
-                                <input :type="type2" placeholder="New password" v-model="userData.password">
+                                <input :type="type2" placeholder="New password" v-model="password">
                                 <div class="input-group-append" style="margin-left:-20px">
                                     <div @click="show2">
                                         <i class="dua fa fa-eye" v-show="display2"></i>
                                         <i v-show="!display2" class="dua fa fa-eye-slash"></i>
                                     </div>
                                 </div>
+                                <span class="validation" v-if="password.length >= 1 && password.length < 6">The password min 6 characters</span>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="input-group">
                                 <i class="satu fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
-                                <input :type="type3" placeholder="Repeat password">
+                                <input :type="type3" placeholder="Repeat password" v-model="$v.repeatPassword.$model">
                                 <div class="input-group-append" style="margin-left:-20px">
                                     <div @click="show3">
                                         <i class="dua fa fa-eye" v-show="display3"></i>
                                         <i v-show="!display3" class="dua fa fa-eye-slash"></i>
                                     </div>
                                 </div>
+                                <span class="validation" v-if="!$v.repeatPassword.sameAsPassword">Password not same</span>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary btn-block" @click.prevent="updatePassword">Change Password</button>
@@ -59,6 +61,7 @@
 import { mapActions, mapGetters } from 'vuex'
 import mixins from '../../components/mixins/swal'
 import axios from 'axios'
+import { required, minLength, sameAs } from 'vuelidate/lib/validators'
 export default {
   name: 'changePw',
   mixins: [mixins],
@@ -72,9 +75,22 @@ export default {
       display2: true,
       display3: true,
       /* User update */
-      userData: {
-        password: ''
-      }
+      //   userData: {
+      //     password: '',
+      //     repeatPassword: ''
+      //   }
+      password: '',
+      repeatPassword: ''
+    }
+  },
+  validations: {
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    repeatPassword: {
+      minLength: minLength(6),
+      sameAsPassword: sameAs('password')
     }
   },
   mounted() {
@@ -101,7 +117,7 @@ export default {
     },
     updatePassword() {
       const id = localStorage.getItem('id')
-      axios.patch(`${process.env.VUE_APP_BASE_URL}/api/v1/user/password/${id}`, { password: this.userData.password })
+      axios.patch(`${process.env.VUE_APP_BASE_URL}/api/v1/user/password/${id}`, { password: this.password })
         .then(res => {
           this.active = !this.active
           this.success('center', 'success', 'Password changed')
@@ -127,6 +143,13 @@ export default {
 }
 .text-wrapper {
     width: 45%;
+}
+
+/* Validation */
+.validation {
+  width: 100%;
+  text-align: center;
+  color: red;
 }
 
 .input-wrapper {
